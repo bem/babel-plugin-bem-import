52ac8a4ada4e3c840ddacf3a68bc6053a341f0bb
Vasiliy Loginevskiy <yeti-or@yandex-team.ru>
2017-02-15 11:10:34 +0300

    Add possibility to import t:tech.
    Refactor and Improve code.

--- index.js
+++ index.js
@@ -28,116 +28,99 @@ return {
             namingOptions = naming || 'react',
             bemNaming = bn(namingOptions);
 
-            debugger;
-            const node = p.node;
-
             if (
-                node.callee.type === 'Identifier' &&
-                node.callee.name === 'require' &&
-                node.arguments[0] && node.arguments[0].value
+                p.node.callee.type === 'Identifier' &&
+                p.node.callee.name === 'require' &&
+                p.node.arguments[0] && p.node.arguments[0].value
             ) {
-                const bemFiles = bemImport
-                    .parse(node.arguments[0].value, path.basename(filename).split('.')[0])
-                    // expand entities by all provided levels
-                    .reduce((acc, entity) => {
-                        levels.forEach(layer => {
-                            // if entity has tech get extensions for it or exactly it,
-                            // otherwise expand entities by default extensions
-                            (
-                                entity.tech ?
-                                    techMap[entity.tech] || [entity.tech] :
-                                    defaultExts
-                            ).forEach(tech => {
-                                acc.push(BemCell.create({ entity, tech, layer }));
-                            });
+
+                const bemFiles = bemImport.parse(
+                    p.node.arguments[0].value,
+                    bemNaming.parse(path.basename(filename).split('.')[0])
+                )
+                // expand entities by all provided levels
+                .reduce((acc, entity) => {
+                    levels.forEach(layer => {
+                        // if entity has tech get extensions for it or exactly it,
+                        // otherwise expand entities by default extensions
+                        (
+                            entity.tech ?
+                                techMap[entity.tech] || [entity.tech] :
+                                defaultExts
+                        ).forEach(tech => {
+                            acc.push(BemCell.create({ entity, tech, layer }));
                         });
-                        return acc;
-                    }, [])
-                    // find path for every entity and check it existance
-                    .map(bemCell => {
-                        const entityPath = path.resolve(
-                                process.cwd(),
-                                bemFs.path(bemCell, namingOptions)
-                            );
-                        // BemFile
-                        return {
-                            cell : bemCell,
-                            exist : fs.existsSync(entityPath),
-                            path : entityPath
-                        };
                     });
+                    return acc;
+                }, [])
+                // find path for every entity and check it existance
+                .map(bemCell => {
+                    const entityPath = path.resolve(
+                            process.cwd(),
+                            bemFs.path(bemCell, namingOptions)
+                        );
 
+                    const pathHack = path.relative(path.dirname(filename), entityPath);
+                    // BemFile
+                    return {
+                        cell : bemCell,
+                        exist : fs.existsSync(entityPath),
+                        path : pathHack
+                    };
+                });
 
-                    if (!bemFiles.length) {
-                        return;
+                if (!bemFiles.length) {
+                    return;
+                }
+
+                /**
+                 * techToFiles:
+                 *   js: [enity, entity]
+                 *   css: [entity, entity, entity]
+                 *   i18n: [entity]
+                 */
+                const techToFiles = {},
+                    existsEntities = {},
+                    errEntities = {};
+
+                bemFiles.forEach(file => {
+                    if(file.exist) {
+                        (techToFiles[file.cell.tech] || (techToFiles[file.cell.tech] = [])).push(file);
+                        existsEntities[file.cell.entity.id] = true;
+
+                        if(file.cell.entity.mod && !file.cell.entity.isSimpleMod()) {
+                            // Add existence for `_mod` if `_mod_val` exists.
+                            existsEntities[BemEntityName.create({
+                                block : file.cell.entity.block,
+                                elem : file.cell.entity.elem,
+                                modName : file.cell.entity.modName
+                            }).id] = true;
+                        }
+                    } else {
+                        existsEntities[file.cell.entity.id] ||
+                            (existsEntities[file.cell.entity.id]  = false);
+                        (errEntities[file.cell.entity.id] ||
+                            (errEntities[file.cell.entity.id] = [])).push(file);
                     }
+                });
 
-
-                    /**
-                     * techToFiles:
-                     *   js: [enity, entity]
-                     *   css: [entity, entity, entity]
-                     *   i18n: [entity]
-                     */
-                    const techToFiles = {},
-                        existsEntities = {},
-                        errEntities = {};
-
-                    bemFiles.forEach(file => {
-                        if(file.exist) {
-                            (techToFiles[file.cell.tech] || (techToFiles[file.cell.tech] = [])).push(file);
-                            existsEntities[file.cell.entity.id] = true;
-
-                            if(file.cell.entity.mod && !file.cell.entity.isSimpleMod()) {
-                                // Add existence for `_mod` if `_mod_val` exists.
-                                existsEntities[BemEntityName.create({
-                                    block : file.cell.entity.block,
-                                    elem : file.cell.entity.elem,
-                                    modName : file.cell.entity.modName
-                                }).id] = true;
-                            }
-                        } else {
-                            existsEntities[file.cell.entity.id] ||
-                                (existsEntities[file.cell.entity.id]  = false);
-                            (errEntities[file.cell.entity.id] ||
-                                (errEntities[file.cell.entity.id] = [])).push(file);
-                        }
-                    });
-
-                    Object.keys(existsEntities).forEach(fileId => {
-                        // check if entity has no tech to resolve
-                        if(!existsEntities[fileId]) {
-                            errEntities[fileId].forEach(file => {
-                                console.warn(`${logSymbols.warning} BEM-Module not found: ${file.path}`);
-                            });
-                        }
-                    });
-
-                    Object.keys(techToFiles).forEach(tech => {
-                        console.log('ext:', tech);
-                        console.log('tech:', extToTech[tech]);
-                        techToFiles[tech].forEach(file => {
-                            console.log(file.cell.entity.id);
+                Object.keys(existsEntities).forEach(fileId => {
+                    // check if entity has no tech to resolve
+                    if(!existsEntities[fileId]) {
+                        errEntities[fileId].forEach(file => {
+                            console.warn(`${logSymbols.warning} BEM-Module not found: ${file.path}`);
                         });
-                    });
-                    // Each tech has own generator
-                    var value = Object.keys(techToFiles).map(tech =>
-                        (generators[extToTech[tech] || tech] || generators['*'])(techToFiles[tech])
-                    ).join('\n')
-
-                    debugger;
-                    console.log(value);
-                    var tmpl = template(value);
-                    p.replaceWith(tmpl());
-
-                    // node.update(
-                    //     // Each tech has own generator
-                    //     Object.keys(techToFiles).map(tech =>
-                    //         (generators[extToTech[tech] || tech] || generators['*'])(techToFiles[tech])
-                    //     ).join('\n')
-                    // );
+                    }
+                });
+                // Each tech has own generator
+                const values = Object.keys(techToFiles).map(tech =>
+                    (generators[extToTech[tech] || tech] || generators['*'])(techToFiles[tech])
+                );
+                if (values.length) {
+                    p.replaceWith(template(values.join('\n'))());
+                }
+            }
         }
-      }
     }
-  };
+};
 }
